9cbb6fd17ab6a4b13d61c929e9da6d947c05f1a0
jinh5 <84804779+jinh5@users.noreply.github.com>
2022-11-05 19:50:34 -0400

    react website commit

--- davinci_discovers-server/src/App.js
+++ davinci_discovers-server/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
+import { NavLink, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1 class="box">DaVinci Discovers</h1>
+    <Navigation />
+    <Main />
+  </div>
+);
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/about'>About</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div className='home'>
+    <h1>Find art that inspires you.</h1>
+    <h1>Find art that inspires you.</h1>
+    <h1>Find art that inspires you.</h1>
+    <h1>Find art that inspires you.</h1>
+  </div>
+);
+
+const About = () => (
+  <div className='about'>
+    <h2>Meet the Team</h2>
+      <p>
+        Hi, we are Team Davinci Discovers. We created this
+        website for our Software Design & Development 
+        class at RPI. We wanted to create a tool for 
+        art enthusiasts to find and learn more about a 
+        artwork or artist they find interesting. We hope 
+        you have enjoy learning and exploring new art you
+        may not have known about before. Happy discovery!
+    </p>
+  </div>
+);
+
+const Main = () => (
+  <Routes>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='./about' component={About}></Route>
+  </Routes>
+);
+
+
 
 export default App;
--- davinci_discovers-server/src/pages/about/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-class About extends React.Component {
-    render() {}
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<About />);
