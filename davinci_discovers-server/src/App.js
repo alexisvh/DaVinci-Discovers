a35ca088506de35e97e68fe5211c6aa4101c9310
jinh5 <84804779+jinh5@users.noreply.github.com>
2022-11-05 22:26:15 -0400

    added navbar component and implemented routing for a few pages

--- davinci_discovers-server/src/App.js
+++ davinci_discovers-server/src/App.js
@@ -1,55 +1,21 @@
 import React from 'react';
-import { NavLink, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+
 
 const App = () => (
-  <div>
+  <Router>
     <h1 class="box">DaVinci Discovers</h1>
-    <Navigation />
-    <Main />
-  </div>
+    <Navbar />
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/about' element={<About/>} />
+    </Routes>
+  </Router>
 );
-
-const Navigation = () => (
-  <nav>
-    <ul>
-      <li><NavLink to='/'>Home</NavLink></li>
-      <li><NavLink to='/about'>About</NavLink></li>
-    </ul>
-  </nav>
-);
-
-const Home = () => (
-  <div className='home'>
-    <h1>Find art that inspires you.</h1>
-    <h1>Find art that inspires you.</h1>
-    <h1>Find art that inspires you.</h1>
-    <h1>Find art that inspires you.</h1>
-  </div>
-);
-
-const About = () => (
-  <div className='about'>
-    <h2>Meet the Team</h2>
-      <p>
-        Hi, we are Team Davinci Discovers. We created this
-        website for our Software Design & Development 
-        class at RPI. We wanted to create a tool for 
-        art enthusiasts to find and learn more about a 
-        artwork or artist they find interesting. We hope 
-        you have enjoy learning and exploring new art you
-        may not have known about before. Happy discovery!
-    </p>
-  </div>
-);
-
-const Main = () => (
-  <Routes>
-    <Route exact path='/' component={Home}></Route>
-    <Route exact path='./about' component={About}></Route>
-  </Routes>
-);
-
-
 
 export default App;
---
+++ davinci_discovers-server/src/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='about'>
+      <h2>Meet the Team</h2>
+        <p>
+          Hi, we are Team Davinci Discovers. We created this
+          website for our Software Design & Development 
+          class at RPI. We wanted to create a tool for 
+          art enthusiasts to find and learn more about a 
+          artwork or artist they find interesting. We hope 
+          you have enjoy learning and exploring new art you
+          may not have known about before. Happy discovery!
+        </p>
+    </div>
+  );
+};
+
+export default About;
