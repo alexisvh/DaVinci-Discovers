d39f6847ac34836aa39c9550ca13e0bb7f430000
jinh5 <84804779+jinh5@users.noreply.github.com>
2022-11-06 01:18:58 -0400

    gallery component commit

--- davinci_discovers-server/src/App.js
+++ davinci_discovers-server/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
+import Home from './pages/Home';
+import About from './pages/About';
+import Feed from './pages/Feed';
 
 
 const App = () => (
@@ -13,7 +14,8 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route exact path='/' element={<Home />} />
-      <Route exact path='/about' element={<About/>} />
+      <Route exact path='/about' element={<About />} />
+      <Route exact path='/feed' element={<Feed />} />
     </Routes>
   </Router>
 );
---
+++ davinci_discovers-server/src/components/Gallery/GalleryElements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Display = styled.div`
+  line-height:0;
+  column-count:5;
+  column-gap:5px;
+`;
+
+export const Image = styled.img`
+  width: auto;
+  height: auto;
+  margin-bottom:5px;
+`;
